400b64fe8c47d0358f6330e59f91ec14565053aa
Ali Irani <aliirani74@gmail.com>
2017-03-20 01:08:19 +0330

    second commit

--- findSimliarFileSizepdfs.sh
+++ findSimliarFileSizepdfs.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-#VAR1 = "test"
-#VAR2 = "TETS"
+
 for f in *.pdf
 do 
 	for j in *.pdf
 	do
-		#echo "$j"
-		#VAR1 = $(stat -c%s "$f")
-		#VAR2 = $(stat -c%s "$j")
 		if [ $(stat -c%s "$f") -eq  $(stat -c%s "$j") ]
 		then
-		#if [ "$VAR1" -eq "$VAR2" ]; then
 			if [ "$f" != "$j" ]
 			then
 				echo $f, $j
